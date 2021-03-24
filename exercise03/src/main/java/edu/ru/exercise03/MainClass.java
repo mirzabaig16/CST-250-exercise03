bf74488aab04e1ccb2e37fbee7fd0215a34c2e83
mirzabaig16 <tayyabmughal05@gmail.com>
2021-03-24 17:23:31 -0500

    completed exercise03

--- exercise03/src/main/java/edu/ru/exercise03/MainClass.java
+++ exercise03/src/main/java/edu/ru/exercise03/MainClass.java
@@ -1,21 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.ru.exercise03;
 
-/**
- *
- * @author Tayyab
- */
-public class MainClass {
+import java.util.*;
 
-    /**
-     * @param args the command line arguments
-     */
+public class MainClass {
+ 
     public static void main(String[] args) {
-        // TODO code application logic here
+    
+       
+       Queue<Integer> myQueue = new LinkedList();
+       Stack<Integer> myStack = new Stack();
+       
+       
+       myQueue.add(1);
+       myQueue.add(2);
+       myQueue.add(3);
+       
+       myStack.add(1);
+       myStack.add(2);
+       myStack.add(3);
+       
+       System.out.println("My Queue: " + myQueue);
+       System.out.println("My Queue: " + myQueue);
+       
+       int head0fQueue = myQueue.remove();
+       int head0fStack = myStack.pop();
+       
+       System.out.println("After removing/popping the head element:");
+       System.out.println("My Queue: " + myQueue);
+       System.out.println("My Queue: " + myQueue);
+      
+    
+       System.out.println(isPalindrome("abcbaxadf"));
+       
+       PriorityQueue<Integer> pQueue = new PriorityQueue();
+       pQueue.add(45);
+       pQueue.add(99);
+       pQueue.add(2);
+       pQueue.add(16);
+       
+       System.out.println("Priority queue: " + pQueue);
+       System.out.println("Head of the pQueue: " + pQueue.peek());
+               
+       
+    }
+    
+    public static Boolean isPalindrome ( String word ){
+        
+        char[] wordArray = word.toCharArray();
+        
+        Queue<Character> wordQueue = new LinkedList();
+        Stack<Character> wordStack = new Stack();
+        
+        for(int i = 0; i<wordArray.length; i++){
+            wordQueue.add(wordArray[i]);
+            wordStack.add(wordArray[i]);
+        }
+        
+        for(int i = 0; i<wordArray.length; i++){
+            if(wordQueue.remove() != wordStack.pop()){
+                return false;
+            }
+        }
+       
+        return true;
     }
     
 }
